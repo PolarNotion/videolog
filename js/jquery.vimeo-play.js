6d69a3533b2b3c7634ebe1ddacd87446591fce1f
Adrianna Valdivia <avaldivi@ggc.edu>
2017-05-24 16:09:03 -0400

    Add 'video' selector for HTML 5 videos to check for play/pause clicks by user with current time

--- js/jquery.vimeo-play.js
+++ js/jquery.vimeo-play.js
@@ -10,9 +10,13 @@
 
     return this.each(function() {
 
+
       if ( settings.loop ) {
+        //VIMEO Video using vAPI
         var iframe = document.querySelector('iframe');
         var player = new Vimeo.Player(iframe);
+
+        //var seconds = player.getCurrentTime();
 
         player.on('pause', function() {
           console.log('The VIMEO video was paused');
@@ -20,6 +24,18 @@
 
         player.on('play', function() {
           console.log('The VIMEO video was played');
+        });
+
+        //HTML 5 Video usng DOM API
+        var video1 = document.querySelector('video');
+
+        $(video1).click(function() {
+          if (video1.paused) {
+            console.log('Video Paused', video1.currentTime);
+          }
+          else if (video1.played) {
+            console.log('Video Playing', video1.currentTime);
+          }
         });
       }
 
