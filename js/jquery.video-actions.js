b95eb180a930bf49335282fa7825c2b79a82fe63
Adrianna Valdivia <avaldivi@ggc.edu>
2017-05-30 15:59:23 -0400

    Remove id to differentiate vimeo video, use class only

--- js/jquery.video-actions.js
+++ js/jquery.video-actions.js
@@ -3,26 +3,23 @@
 
     // Establish our default settings
     var settings = $.extend({}, $.fn.videoActions.defaults, options);
-    var v_players={};
-    var id
 
     return $(".vimeo").each(function() {
 
           //VIMEO Video using vAPI
           //var iframe = document.querySelector('iframe');
-          var id = this.id;
-          v_players[id] = new Vimeo.Player(id);
+          var player = new Vimeo.Player(this);
 
-          v_players[id].on('pause', function() {
-            v_players[id].getCurrentTime().then(function(seconds) {
+          player.on('pause', function() {
+            player.getCurrentTime().then(function(seconds) {
               console.log("Vimeo video paused at: " + seconds);
             }).catch(function(error) {
               console.log("There was an error");
             });
           });
 
-          v_players[id].on('play', function() {
-            v_players[id].getCurrentTime().then(function(seconds) {
+          player.on('play', function() {
+            player.getCurrentTime().then(function(seconds) {
               console.log("Vimeo video played at: " + seconds);
             }).catch(function(error) {
               console.log("There was an error");
