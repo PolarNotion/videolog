9c69d6027d8b151c9009b5d5010cf18a8ad3f88c
Adrianna Valdivia <avaldivi@ggc.edu>
2017-05-31 16:41:36 -0400

    Handle multiple HTML5 Videos by their id

--- js/jquery.video-actions.js
+++ js/jquery.video-actions.js
@@ -1,8 +1,12 @@
 (function($) {
   $.fn.videoActions = function(options) {
 
+    var video_player={};
+    var id = this.id;
+
     // Establish our default settings
     var settings = $.extend({}, $.fn.videoActions.defaults, options);
+
 
     return this.each( function() {
 
@@ -30,18 +34,24 @@
           //END of Vimeo video
       });
 
-      //HTML 5 Video using DOM API
-      var video1 = document.querySelector('video');
+      $(".video").each(function() {
 
-      $(video1).click(function() {
-        if (video1.paused) {
-          console.log('HTML5 Video played at: ', video1.currentTime);
-        }
-        else if (video1.played) {
-          console.log('HTML5 Video paused at: ', video1.currentTime);
-        }
+        var video_player={};
+        var id = this.id;
+
+      video_player[id] = document.getElementById(id);
+
+        $(video_player[id]).click(function() {
+          if (video_player[id].paused) {
+            console.log('HTML5 Video played at: ', video1.currentTime);
+          }
+          else if (video_player[id].played) {
+            console.log('HTML5 Video paused at: ', video1.currentTime);
+          }
+        });
+        //END of HTML 5 Video
+
       });
-      //END of HTML 5 Video
 
     });
 
