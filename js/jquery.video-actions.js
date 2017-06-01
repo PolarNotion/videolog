ea2d777eab515ec94fb22bc4e79cf24635b2b519
Adrianna Valdivia <avaldivi@ggc.edu>
2017-06-01 15:28:52 -0400

    Make onYoutubeIframeAPIReady global, add iframe api script

--- js/jquery.video-actions.js
+++ js/jquery.video-actions.js
@@ -50,6 +50,18 @@
 
       });
 
+      window.onYouTubeIframeAPIReady = function() {
+
+        });
+
+      }
+
+      var tag = document.createElement('script');
+      tag.id = 'iframe-demo';
+      tag.src = 'https://www.youtube.com/iframe_api';
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
     });
 
   };
