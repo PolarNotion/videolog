fc3521b2f3ae7afbb73ffd0cdd67a28b4aac6684
Adrianna Valdivia <avaldivi@ggc.edu>
2017-06-05 23:07:52 -0400

    Loop through array of iframe elements that contain 'vimeo' in src attribute

--- js/jquery.video-actions.js
+++ js/jquery.video-actions.js
@@ -9,92 +9,28 @@
     }, options);
 
 
-    return this.each( function() {
+        return this.each(function() {
+            var videoVids = document.body.querySelectorAll('video');
 
         $(".vimeo").each(function() {
 
-          // //VIMEO Video using vAPI
-          //
-          //   if (document.querySelector('iframe') {
-          //     // check if url has vimeo or youtube word in url
-          //     // if url == youtube
-          //       // run youtube code
-          //     // else if url == vimeo
-          //     // run vimeo code
-          //   }
-          var id = this.id;
-          var player = new Vimeo.Player(id);
+            //Loop through HTML5 videos
+            for (i = 0; i < videoVids.length; i++) {
 
-          player.on('pause', function() {
-            player.getCurrentTime().then(function(seconds) {
-              console.log('Vimeo video ' + id + ' paused at: ' + seconds);
-            }).catch(function(error) {
-              console.log("There was an error");
-            });
-          });
+                if (this === videoVids[i]) {
 
-          player.on('play', function() {
-            player.getCurrentTime().then(function(seconds) {
-              console.log('Vimeo video ' + id + ' played at: ' + seconds);
-            }).catch(function(error) {
-              console.log("There was an error");
-            });
-          });
-          //END of Vimeo video
-      });
+                    var id = this.id;
+                    var video_player = document.getElementById(id);
 
-      $(".video").each(function() {
-
-        //var video_player={};
-        var id = this.id;
-        var video_player = document.getElementById(id);
-
-        $(video_player).click(function() {
-          if (video_player.paused) {
-            console.log('HTML5 Video ' + id + ' played at: ' + video1.currentTime);
-          }
-          else if (video_player.played) {
-            console.log('HTML5 Video ' + id + ' paused at: ' + video1.currentTime);
-          }
-        });
-        //END of HTML 5 Video
-
-      });
-
-      window.onYouTubeIframeAPIReady = function() {
-
-        $(".youtube").each(function() {
-
-          // BEGINNING OF YOUTUBE SETTINGS
-          //debugger;
-          var id = this.id;
-          var ytplayer;
-          ytplayer = new YT.Player(id, {
-              events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-              }
-          });
-
-          function onPlayerReady(event) {
-
-            document.getElementById(id);
-          }
-
-          function getStatus(playerStatus) {
-            //debugger;
-            if (playerStatus == -1) {
-              console.log("Not yet started"); // unstarted = gray
-            } else if (playerStatus == 0) {
-              console.log("Youtube video", id, "has ended at ", ytplayer.getCurrentTime());
-            } else if (playerStatus == 1) {
-              console.log("Youtube video ", id, " is playing at ", ytplayer.getCurrentTime());
-            } else if (playerStatus == 2) {
-              console.log("Youtube video", id, "has been paused at ", ytplayer.getCurrentTime());
-            } else if (playerStatus == 3) {
-              console.log("Youtube video", id, "is buffering at ", ytplayer.getCurrentTime());
-            } else if (playerStatus == 5) {
-              console.log("Youtube video", id, "was cued at ", ytplayer.getCurrentTime());
+                    $(video_player).click(function() {
+                        if (video_player.paused) {
+                            console.log('HTML5 Video ' + id + ' played at: ' + video1.currentTime);
+                        } else if (video_player.played) {
+                            console.log('HTML5 Video ' + id + ' paused at: ' + video1.currentTime);
+                        }
+                    });
+                    //END of HTML 5 Video
+                }
             }
           }
 
